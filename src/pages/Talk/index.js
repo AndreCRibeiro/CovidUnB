692d395fb91d97bbc3bbdc9bc2d63cfd27fd0c88
André Cavalcanti Ribeiro <andre_ribeiro97@hotmail.com>
2020-04-01 18:16:29 -0300

    feat/talkPage

--- src/pages/Talk/index.js
+++ src/pages/Talk/index.js
@@ -1,90 +1,29 @@
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/static-property-placement */
 /* eslint-disable global-require */
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
-import { ActivityIndicator } from 'react-native';
-import PropTypes from 'prop-types';
 
 import {
   Container,
-  Logo,
-  Form,
-  Input,
-  ButtonLogin,
-  LoginButtonText,
-  Slash,
-  SignupButtonText,
-  LineLeft,
-  LineRight,
+  CenterView,
+  LogoUnB,
+  LogoLatitude,
   SimpleText,
-  ButtonSingup,
 } from './styles';
 
 export default class Talk extends Component {
-  static propTypes = {
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func,
-    }).isRequired,
-  };
-
-  state = {
-    userMail: '',
-    userPass: '',
-    loading: false,
-  };
-
-  handleLogin = () => {
-    const { navigation } = this.props;
-
-    navigation.navigate('Home');
-  };
-
-  handleNavigationToSignUp = () => {
-    const { navigation } = this.props;
-
-    navigation.navigate('Register');
-  };
-
   render() {
-    const { userMail, userPass, loading } = this.state;
-
     return (
       <Container>
-        <Logo source={require('../../assets/images/logo.png')} />
-        <Form>
-          <Input
-            autoCorrect={false}
-            autoCapitalize="none"
-            placeholder="Email"
-            value={userMail}
-            onChangeText={(text) => this.setState({ userMail: text })}
-          />
-          <Input
-            autoCorrect={false}
-            autoCapitalize="none"
-            placeholder="Senha"
-            value={userPass}
-            onChangeText={(text) => this.setState({ userPass: text })}
-            returnKeyType="send"
-            onSubmitEditing={this.handleLogin}
-          />
-
-          <ButtonLogin loading={loading} onPress={this.handleLogin}>
-            {loading ? (
-              <ActivityIndicator color="#fff" />
-            ) : (
-                <LoginButtonText>Login</LoginButtonText>
-              )}
-          </ButtonLogin>
-          <Slash>
-            <LineLeft />
-            <SimpleText>OU</SimpleText>
-            <LineRight />
-          </Slash>
-          <ButtonSingup onPress={this.handleNavigationToSignUp}>
-            <SignupButtonText>Registre-se</SignupButtonText>
-          </ButtonSingup>
-        </Form>
+        <CenterView>
+          <LogoUnB source={require('../../assets/images/logo.png')} />
+          <SimpleText>Telefone: (61)XXXX-XXXX</SimpleText>
+        </CenterView>
+        <CenterView>
+          <LogoLatitude source={require('../../assets/images/latitude.png')} />
+          <SimpleText>Telefone: (61)YYYY-YYYY</SimpleText>
+        </CenterView>
       </Container>
     );
   }
