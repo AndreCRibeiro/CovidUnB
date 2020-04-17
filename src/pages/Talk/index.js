f4127eead41aada899f6ee61d95d2af14e49cd40
André Cavalcanti Ribeiro <andre_ribeiro97@hotmail.com>
2020-04-16 21:15:33 -0300

    fix:removeTel

--- src/pages/Talk/index.js
+++ src/pages/Talk/index.js
@@ -33,11 +33,9 @@ export default class Talk extends Component {
       <Container>
         <CenterView>
           <LogoUnB source={require('../../assets/images/logo.png')} />
-          <SimpleText>Telefone: (61) 3107-3300</SimpleText>
         </CenterView>
         <CenterView>
           <LogoLatitude source={require('../../assets/images/latitude.png')} />
-          <SimpleText>Telefone: (61) 3107-5597</SimpleText>
         </CenterView>
       </Container>
     );
