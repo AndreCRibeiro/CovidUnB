50d3584ea19323809966e40812611efee027cd34
zerbinidamata <rafinhazerbini@gmail.com>
2020-04-26 23:24:46 -0300

    map fix

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, BackHandler } from 'react-native';
 import MapView from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import { ActivityIndicator } from 'react-native-paper';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 export default class Map extends Component {
   state = {
@@ -22,7 +21,7 @@ export default class Map extends Component {
             longitudeDelta: 0.0134,
           },
         });
-      }, //sucesso
+      }, // sucesso
       () => {
         this.setState({
           mapAvaible: false,
@@ -40,78 +39,31 @@ export default class Map extends Component {
         maximumAge: 1000,
       }
     );
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
   }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  handleBackButton = () => {
+    const { navigation } = this.props;
+    navigation.navigate('Home');
+    return true;
+  };
 
   setLocation(coord) {}
 
   render() {
     const { region, mapAvaible } = this.state;
-    const homePlace = {
-      description: 'Home',
-      geometry: { location: { lat: 48.8152937, lng: 2.4597668 } },
-    };
-    const workPlace = {
-      description: 'Work',
-      geometry: { location: { lat: 48.8496818, lng: 2.2940881 } },
-    };
-
     return (
       <View style={{ flex: 1 }}>
         {region ? (
-          <GooglePlacesAutocomplete
-            placeholder="Search"
-            minLength={2} // minimum length of text to search
-            autoFocus={false}
-            returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-            keyboardAppearance={'light'} // Can be left out for default keyboardAppearance https://facebook.github.io/react-native/docs/textinput.html#keyboardappearance
-            listViewDisplayed="auto" // true/false/undefined
-            fetchDetails={true}
-            renderDescription={(row) => row.description} // custom description render
-            onPress={(data, details = null) => {
-              // 'details' is provided when fetchDetails = true
-              console.log(data, details);
-            }}
-            getDefaultValue={() => ''}
-            query={{
-              // available options: https://developers.google.com/places/web-service/autocomplete
-              key: 'AIzaSyCA3oGCY61JLGgp7W122FGDa9sU0awFIZYY',
-              language: 'en', // language of the results
-              types: '(cities)', // default: 'geocode'
-            }}
-            styles={{
-              textInputContainer: {
-                width: '100%',
-              },
-              description: {
-                fontWeight: 'bold',
-              },
-              predefinedPlacesDescription: {
-                color: '#1faadb',
-              },
-            }}
-            currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-            currentLocationLabel="Current location"
-            nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-            GoogleReverseGeocodingQuery={
-              {
-                // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-              }
-            }
-            GooglePlacesSearchQuery={{
-              // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-              rankby: 'distance',
-              type: 'cafe',
-            }}
-            GooglePlacesDetailsQuery={{
-              // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
-              fields: 'formatted_address',
-            }}
-            filterReverseGeocodingByTypes={[
-              'locality',
-              'administrative_area_level_3',
-            ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-            predefinedPlaces={[homePlace, workPlace]}
-            debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+          <MapView
+            style={{ flex: 1 }}
+            region={region}
+            showsUserLocation
+            loadingEnabled
           />
         ) : (
           <ActivityIndicator />
